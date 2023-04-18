40a524043ebe9f34c18dfdc0c5e0ff2d83a737a3
brian <braintm2@gmail.com>
2023-04-18 13:22:44 -0700

    2nd test

--- github-actions/copy-file-to-new-repo.js
+++ github-actions/copy-file-to-new-repo.js
@@ -1,8 +1,12 @@
-const BASE_URL = 'https://api.github.com/repos'
-const repo = process.env.INPUT_REPO;
-const path = process.env.INPUT_PATH;
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-async function updateCodeOfConduct(repo, path) {
+const BASE_URL = 'https://api.github.com/repos'
+const repo = core.getInput('repo');
+const path = core.getInput('path');
+const token = core.getInput('token');
+
+async function updateCodeOfConduct() {
     const codeOfConductSource = await fetch(`${BASE_URL}/bootcamp-brian/github-actions-testing-2/contents/README.md`, {
       method: "GET",
       headers: {
@@ -35,9 +39,9 @@ async function updateCodeOfConduct(repo, path) {
       headers: {
         'Accept': 'application/vnd.github+json',
         'X-GitHub-Api-Version': '2022-11-28',
-        'Authorization': `Bearer ${ process.env.token }`
+        'Authorization': `Bearer ${ token }`
       }
     });
 }
 
-updateCodeOfConduct(repo, path);
+updateCodeOfConduct();
