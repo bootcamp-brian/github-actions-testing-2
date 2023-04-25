d2bd50ae781c242b7f1346cac8aa2df70eba0529
Brian Mui <113788952+bootcamp-brian@users.noreply.github.com>
2023-04-25 13:05:30 -0700

    Update copy-file-to-new-repo.js

--- github-action/copy-file-to-new-repo.js
+++ github-action/copy-file-to-new-repo.js
@@ -5,36 +5,48 @@ const octokit = new Octokit({ auth: token });
 
 // Copies contents of code of conduct source file and copies them over to a target repo
 // Needs arguments repo: 'name of target repo', path: 'path of file to copy contents into'
-async function updateCodeOfConduct(repo, path) {
+async function updateCodeOfConduct(repos) {
     const owner = 'bootcamp-brian';
     const codeOfConductSource = await octokit.request(`GET /repos/${owner}/github-actions-testing-2/contents/README.md`);
 
     const { content } = codeOfConductSource.data;
 
-    let currentCodeOfConduct = false;
-    
-    try {
-        currentCodeOfConduct = await octokit.request(`GET /repos/${owner}/${repo}/contents/${path}`);
-    } catch {
-        console.log('there was an error')
-    }
+    for (let repo of repos) {
+        const { name, path } = repo;
+        let currentCodeOfConduct = false;
 
-    console.log(currentCodeOfConduct);
+        try {
+            currentCodeOfConduct = await octokit.request(`GET /repos/${owner}/${name}/contents/${path}`);
+        } catch {
+            console.log('there was an error')
+        }
 
-    if (currentCodeOfConduct) {
-        const { sha } = currentCodeOfConduct.data;
-        const response = await octokit.request(`PUT /repos/${owner}/${repo}/contents/${path}`, {
-            sha,
-            message: 'Updating code of conduct',
-            content
-        });
-    } else {
-        const response = await octokit.request(`PUT /repos/${owner}/${repo}/contents/${path}`, {
-            message: 'Updating code of conduct',
-            content
-        });
+        if (currentCodeOfConduct) {
+            const { sha } = currentCodeOfConduct.data;
+            const response = await octokit.request(`PUT /repos/${owner}/${name}/contents/${path}`, {
+                sha,
+                message: 'Updating code of conduct',
+                content
+            });
+        } else {
+            const response = await octokit.request(`PUT /repos/${owner}/${name}/contents/${path}`, {
+                message: 'Updating code of conduct',
+                content
+            });
+        }
     }
 }
 
+const repos = [
+    {
+        name: 'github-actions-testing1',
+        path: '_includes/code-of-conduct-page/CODEOFCONDUCT.md'
+    },
+    {
+        name: 'github-actions-testing-2',
+        path: '_includes/code-of-conduct-page/CODEOFCONDUCT.md'
+    },
+]
+
 // Run updateCodeofConduct() for all repos that need a copy of the code of conduct
-updateCodeOfConduct('github-actions-testing1', '_includes/code-of-conduct-page/CODEOFCONDUCT.md');
+updateCodeOfConduct(repos);
