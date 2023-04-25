b3915a626cf6048d0fcf4558a60a10c30cb6f147
Brian Mui <113788952+bootcamp-brian@users.noreply.github.com>
2023-04-25 12:26:57 -0700

    Update copy-file-to-new-repo.js

--- github-action/copy-file-to-new-repo.js
+++ github-action/copy-file-to-new-repo.js
@@ -6,44 +6,47 @@ const octokit = new Octokit({ auth: token });
 // Copies contents of code of conduct source file and copies them over to a target repo
 // Needs arguments repo: 'name of target repo', path: 'path of file to copy contents into'
 async function updateCodeOfConduct(repo, path) {
-    const codeOfConductSource = await octokit.rest.repos.getContent({
-        owner: 'bootcamp-brian',
-        repo: 'github-actions-testing-2',
-        path: 'README.md',
-    });
+    try {
+        const codeOfConductSource = await octokit.rest.repos.getContent({
+            owner: 'bootcamp-brian',
+            repo: 'github-actions-testing-2',
+            path: 'README.md',
+        });
 
-    const { content } = codeOfConductSource.data;
-    
-    console.log('this line of code runs');
-    
-    const currentCodeOfConduct = await octokit.rest.repos.getContent({
-        owner: 'bootcamp-brian',
-        repo,
-        path,
-    });
+        const { content } = codeOfConductSource.data;
 
-    console.log(currentCodeOfConduct);
-    
-    if (currentCodeOfConduct.data.sha) {
-        const { sha } = currentCodeOfConduct.data;
-        const response = await octokit.rest.repos.createOrUpdateFileContents({
+        console.log('this line of code runs');
+
+        const currentCodeOfConduct = await octokit.rest.repos.getContent({
             owner: 'bootcamp-brian',
             repo,
             path,
-            message: 'Updating code of conduct',
-            content,
-            sha,
         });
-    } else {
-        const response = await octokit.rest.repos.createOrUpdateFileContents({
-            owner: 'bootcamp-brian',
-            repo,
-            path,
-            message: 'Updating code of conduct',
-            content,
-        });
+
+        console.log(currentCodeOfConduct);
+
+        if (currentCodeOfConduct.data.sha) {
+            const { sha } = currentCodeOfConduct.data;
+            const response = await octokit.rest.repos.createOrUpdateFileContents({
+                owner: 'bootcamp-brian',
+                repo,
+                path,
+                message: 'Updating code of conduct',
+                content,
+                sha,
+            });
+        } else {
+            const response = await octokit.rest.repos.createOrUpdateFileContents({
+                owner: 'bootcamp-brian',
+                repo,
+                path,
+                message: 'Updating code of conduct',
+                content,
+            });
+        }
+    } catch (error) {
+        console.error(error);
     }
-    
 }
 
 // Run updateCodeofConduct() for all repos that need a copy of the code of conduct
